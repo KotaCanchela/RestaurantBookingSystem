84dfef1368f75d7f144ae17e646e103666928871
KotaCanchela <dakotacanchela@gmail.com>
2021-03-09 18:07:18 +0000

    First commit

---
+++ app/library/build/generated/source/buildConfig/debug/com/iammert/library/readablebottombar/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.iammert.library.readablebottombar;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.iammert.library.readablebottombar";
+  public static final String BUILD_TYPE = "debug";
+}
