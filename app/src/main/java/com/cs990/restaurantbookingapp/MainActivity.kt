d554be3f203880b589f8cc2303be586a5cfd2fec
Ali Pollock <alimpollock@gmail.com>
2021-03-09 19:40:35 +0000

    ViewBindings

--- app/src/main/java/com/cs990/restaurantbookingapp/MainActivity.kt
+++ app/src/main/java/com/cs990/restaurantbookingapp/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.cs990.restaurantbookingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.cs990.restaurantbookingapp.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,5 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         bottomNavigationView.setupWithNavController(navController)
+
+
     }
 }
