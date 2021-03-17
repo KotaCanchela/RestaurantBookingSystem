05472f162df77fde247d28f6a4e1d17f64f15235
Ali Pollock <alimpollock@gmail.com>
2021-03-17 18:20:43 +0000

    AddBookingsToDatabase

--- app/src/main/java/com/cs990/restaurantbookingapp/models/BookingItem.kt
+++ app/src/main/java/com/cs990/restaurantbookingapp/models/BookingItem.kt
@@ -1,12 +1,41 @@
 package com.cs990.restaurantbookingapp.models
 
-class BookingItem(    var restaurantItem: RestaurantItem,
-                      var day: String,
-                      var month: String,
-                      var year: String,
-                      var hour: String,
-                      var minute: String) {
+class BookingItem(restaurantItem: RestaurantItem, guestNumber: String, day: String, month: String, year: String, hour: String, minute: String) {
+
+    private var restaurantItem: RestaurantItem = restaurantItem
+    private var guestNumber: String = guestNumber
+    private var day: String = day
+    private var month: String = month
+    private var year: String = year
+    private var hour: String = hour
+    private var minute: String = minute
 
 
 
+    fun getRestaurant(): RestaurantItem{
+        return this.restaurantItem
+    }
+    fun getGuestNumber(): String {
+        return this.guestNumber
+    }
+
+    fun getDay(): String{
+        return this.day
+    }
+
+    fun getMonth(): String{
+        return this.month
+    }
+
+    fun getYear(): String{
+        return this.year
+    }
+
+    fun getHour(): String{
+        return this.hour
+    }
+
+    fun getMinute(): String{
+        return this.minute
+    }
 }
