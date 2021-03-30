327ee1c74ba709d433f89f543868737310ccf26e
Andrew <knockglassfarm@gmail.com>
2021-03-30 15:09:22 +0100

    Added further commenting to the profiles folder

--- app/src/main/java/com/cs990/restaurantbookingapp/profilePages/MyPaymentActivity.kt
+++ app/src/main/java/com/cs990/restaurantbookingapp/profilePages/MyPaymentActivity.kt
@@ -4,8 +4,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.cs990.restaurantbookingapp.R
 
+/**
+ *  An Android Layout Class for user Payment Activity
+ * @author Group 1
+ * @version 1.0
+ */
 class MyPaymentActivity : AppCompatActivity()
 {
+    /**
+     * onCreate method for this Layout.
+     */
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
