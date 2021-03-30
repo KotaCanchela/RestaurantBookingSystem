bb814e5cc068d29d37cac1f2feed6960c07866c9
Andrew <knockglassfarm@gmail.com>
2021-03-30 14:40:08 +0100

    Added commenting and removed unused Class (restaurantitems)

--- app/src/main/java/com/cs990/restaurantbookingapp/adapters/RestaurantCuisineHomeAdapter.kt
+++ app/src/main/java/com/cs990/restaurantbookingapp/adapters/RestaurantCuisineHomeAdapter.kt
@@ -17,6 +17,11 @@ import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import kotlinx.android.synthetic.main.card_home_cuisine.view.*
 
+/**
+ *  An Adapter Class that provides functionality for Restaurant Cuisine Items.
+ * @author Group 1
+ * @version 1.0
+ */
 class RestaurantCuisineHomeAdapter(
     val context: Context,
     val options: FirestoreRecyclerOptions<RestaurantItem>
@@ -24,15 +29,15 @@ class RestaurantCuisineHomeAdapter(
     FirestoreRecyclerAdapter<RestaurantItem, RestaurantCuisineHomeAdapter.RestaurantViewHolder>(
         options) {
 
-
     class RestaurantViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val restaurantNameText: TextView = itemView.findViewById(R.id.tv_restaurantCuisine_home)
         val restaurantImageItem: ImageView = itemView.findViewById(R.id.iv_home_cuisine)
     }
 
+    /**
+     * onCreate method for this Class. Inflates the item views which is designed in xml layout file
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RestaurantViewHolder {
-
-
         return RestaurantViewHolder(
             LayoutInflater.from(parent.context).inflate(
                 R.layout.card_home_cuisine,
@@ -42,15 +47,21 @@ class RestaurantCuisineHomeAdapter(
         )
     }
 
-
-
+    /**
+     * Binds each item in the ArrayList to a view
+     *
+     * Called when RecyclerView needs a new {@link ViewHolder} of the given type to represent
+     * an item.
+     *
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     */
     override fun onBindViewHolder(
         holder: RestaurantViewHolder,
         position: Int,
         model: RestaurantItem
     ) {
-
-
         holder.restaurantNameText.tv_restaurantCuisine_home.text = model.getCuisine()
 
         val url = model.getRestaurantImage()
@@ -58,8 +69,6 @@ class RestaurantCuisineHomeAdapter(
             .with(holder.restaurantImageItem)
             .load(url)
             .into(holder.restaurantImageItem.iv_home_cuisine)
-
-
         holder.itemView.setOnClickListener {
 
             val intent = Intent(context, RestaurantPageActivity::class.java)
@@ -68,4 +77,3 @@ class RestaurantCuisineHomeAdapter(
         }
     }
 }
-
